c9cecad7ccb6c4960dbd9008a166bcbe036a00ee
Rupal <rupalgupta951@gmail.com>
2026-06-09 00:35:38 +0530

    added backed prod url to avoid Cors error

--- Backend/app/app.js
+++ Backend/app/app.js
@@ -3,6 +3,11 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
+    credentials: true,
+  }),
+);
 
 module.exports = app;
